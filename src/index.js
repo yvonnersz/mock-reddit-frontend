752fbf7281cdbb3a15d00a1359ad702212506088
Yvonne <yvonnersz@gmail.com>
2020-10-17 05:36:18 -0700

    import BrowserRouter to set up routes

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import postsReducer from './reducers/postsReducer';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 const store = createStore(postsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
