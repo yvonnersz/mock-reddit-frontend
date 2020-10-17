2b0f9d33f3d753bd4a9ccc9801ec5b526a890b60
Yvonne <yvonnersz@gmail.com>
2020-10-17 00:32:51 -0700

    create PostInput component

---
+++ src/components/PostInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostInput extends React.Component {
+    render() {
+        return (
+            <div>
+                Post Input
+            </div>
+        )
+    }
+}
+
+export default PostInput;
--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PostInput from '../components/PostInput';
 
 class PostsContainer extends React.Component {
     render() {
         return (
             <div>
-                PostsContainer
+                <PostInput />
             </div>
         )
     }
