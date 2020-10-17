62b6b7752f254ce3e64c21d84f79c70b339cac15
Yvonne <yvonnersz@gmail.com>
2020-10-17 02:59:07 -0700

    import components and connect

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Posts = (props) => {
     return (
         <div>
-
+            Posts
         </div>
     )
 }
--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import PostInput from '../components/PostInput';
+import Posts from '../components/Posts';
+import { connect } from 'react-redux';
 
 class PostsContainer extends React.Component {
     render() {
         return (
             <div>
                 <PostInput />
+                <Posts />
             </div>
         )
     }
 }
 
-export default PostsContainer;
+const mapStateToProps = state => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect (mapStateToProps)(PostsContainer);
