47709182c2b8a827a3d7489568dabb33773212c1
Yvonne <yvonnersz@gmail.com>
2020-10-19 17:57:40 -0700

    adding routes

--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -3,6 +3,7 @@ import PostInput from '../components/PostInput';
 import Posts from '../components/Posts';
 import { connect } from 'react-redux';
 import {fetchPosts} from '../actions/fetchPosts';
+import {Route} from 'react-router-dom';
 
 class PostsContainer extends React.Component {
     componentDidMount() {
@@ -12,8 +13,8 @@ class PostsContainer extends React.Component {
     render() {
         return (
             <div>
-                <PostInput />
-                <Posts posts={this.props.posts} />
+                <Route path='/posts/new' component={PostInput} />
+                <Route exact path='/posts' render= {() => <Posts posts={this.props.posts} />} />
             </div>
         )
     }
