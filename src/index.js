2d3e4721ef34bd9cfec77e5f66560236fa49d108
Yvonne <yvonnersz@gmail.com>
2020-10-19 21:12:23 -0700

    import compose

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import postsReducer from './reducers/postsReducer';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-const store = createStore(postsReducer, applyMiddleware(thunk))
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(postsReducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
