9c673c97247d762673eeba82508faa955fe08575
Yvonne <yvonnersz@gmail.com>
2020-10-20 06:22:49 -0700

    add comment feature now works

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -6,9 +6,7 @@ export default function postsReducer(state = { posts: [] }, action) {
             return {...state, posts: [...state.posts, action.payload]}
         case 'ADD_COMMENT':
             let post = state.posts.filter(post => post.id === action.payload.post_id)[0]
-
             let newPost = {...post, comments: [...post.comments, action.payload]}
-            
             return {...state, posts: [newPost]}
         default:
             return state
