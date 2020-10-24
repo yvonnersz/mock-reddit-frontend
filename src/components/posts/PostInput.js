52d7ad857f91bbd331248933694c79b1ff6d64e9
Yvonne <yvonnersz@gmail.com>
2020-10-24 02:21:59 -0700

    css for PostInput component

--- src/components/posts/PostInput.js
+++ src/components/posts/PostInput.js
@@ -9,6 +9,7 @@ class PostInput extends React.Component {
         this.state = {
             title: '',
             subreddit: '',
+            user: '',
             content: '',
             upvotes: 0
         }
@@ -26,20 +27,32 @@ class PostInput extends React.Component {
         this.setState({
             title: '',
             subreddit: '',
+            user: '',
             content: ''
         })
     }
 
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleOnSubmit}>
-                    <input type='text' placeholder='Title' value={this.state.title} name='title' onChange={this.handleOnChange} /><br/>
-                    <input type='text' placeholder='Content' value={this.state.content} name='content' onChange={this.handleOnChange} /><br/>
-                    <input type='text' placeholder='Subreddit' value={this.state.subreddit} name='subreddit' onChange={this.handleOnChange} /><br/>
-                    <input type='submit' value="Create Post" /><br/>
-                </form>
+            <div class='container-fluid text-center'>
+                <h2>Create a Post</h2>
+                <div class='row content new-post'>
+                    <div class='form-group'>
+                        <form onSubmit={this.handleOnSubmit}>
+                            <input type='text' placeholder='Title' value={this.state.title} name='title' onChange={this.handleOnChange} class="form-control"/><br/>
+
+                            <input type='text' placeholder='Subreddit' value={this.state.subreddit} name='subreddit' onChange={this.handleOnChange} class="form-control"/><br/>
+
+                            <input type='text' placeholder='User' value={this.state.user} name='user' onChange={this.handleOnChange} class="form-control"/><br/>
+
+                            <textarea rows="7" placeholder='Content' value={this.state.content} name='content' onChange={this.handleOnChange} class="form-control"/><br/>
+
+                            <input type='submit' value="Create Post" /><br/>
+                        </form>
+                    </div>
+                </div>
             </div>
+
         )
     }
 }
