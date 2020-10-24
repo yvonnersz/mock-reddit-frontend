2ef576727d27f0202679de114b35dffc96cb36db
Yvonne <yvonnersz@gmail.com>
2020-10-23 22:13:09 -0700

    css container addition

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -10,15 +10,17 @@ const Posts = (props) => {
         <div>
             {props.posts.map(post => 
                 <div class='container-fluid text-center' key={post.id}>
-                    <div class='col-lg-8 text-left'>
+                    <div class="row content">
+                        <div class='col-lg-8 text-left'>
 
-                        <span>r/{post.subreddit} • Posted by u/{post.user} • {post.created_at}</span>
-                        <Link to={`/posts/${post.id}`}>
-                            <h3>{post.title}</h3>
-                        </Link>
-                        <p>{post.content}</p>
-                        <span><Pluralize singular={'Comment'} count={post.comments.length} /></span>
+                            <span>r/{post.subreddit} • Posted by u/{post.user} • {post.created_at}</span>
+                            <Link to={`/posts/${post.id}`}>
+                                <h3>{post.title}</h3>
+                            </Link>
+                            <p>{post.content}</p>
+                            <span><Pluralize singular={'Comment'} count={post.comments.length} /></span>
 
+                        </div>
                     </div>
                 </div>
             )}
