dd5993dc56d3ca34bd18288ba711b16bd30acb2f
Yvonne <yvonnersz@gmail.com>
2020-10-25 05:56:01 -0700

    cleaning up code

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -2,18 +2,6 @@ import React from 'react';
 
 import Comment from './Comment';
 
-// const Comments = (props) => {
-//     console.log(props.posts)
-//     return (
-//         <div>
-//             <h3>Comments</h3>
-            // {/* {props.posts && props.posts.map(comment => <Comment key={comment.id} comment={comment} />)} */}
-
-//             {/* I NEED TO GRAB ID OF URL */}
-//         </div>
-//     )    
-// }
-
 class Comments extends React.Component {
     render() {
         let postId = parseInt(this.props.postRouterId)
