5ff0d2cde4322c57a152ade28ee5e89f0ddb304d
Yvonne <yvonnersz@gmail.com>
2020-10-29 13:04:54 -0700

    refactor reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -5,44 +5,34 @@ export default function postsReducer(state = { posts: [] }, action) {
         case 'ADD_POST':
             let addPost = {...action.payload, comments: []}
             return {...state, posts: [...state.posts, addPost]}
-        case 'ADD_COMMENT':
-            let statepost = state.posts.filter(post => post.id === action.payload.post_id)[0]
-            let newlyupdatedpost = {...statepost, upvotes: action.payload.post.upvotes, comments: [...statepost.comments, action.payload]}
-            let indexpost = state.posts.findIndex(post => post.id === action.payload.post_id)
-            let slice1 = state.posts.slice(0, indexpost)
-            let slice2 = state.posts.slice(indexpost + 1)
-            let newarray = slice1.concat(newlyupdatedpost).concat(slice2)
-            return {...state, posts: newarray}
         case 'EDIT_POST':
-            let oldobjectindex = state.posts.findIndex(post => post.id === action.payload.id)
-            let beginningofarray = state.posts.slice(0, oldobjectindex)
-            let endofarray = state.posts.slice(oldobjectindex + 1)
-            let neweditpostarray = beginningofarray.concat(action.payload).concat(endofarray)
-            return {...state, posts: neweditpostarray}
-        case 'EDIT_COMMENT':
-             let desiredpost2 = state.posts.filter(post => post.id === action.payload.post_id)[0]
-             let commentindex2 = desiredpost2.comments.findIndex(comment => comment.id === action.payload.id)
-             let commentbegin2 = desiredpost2.comments.slice(0, commentindex2)
-             let commentend2 = desiredpost2.comments.slice(commentindex2 + 1)
-             let newcomment2 = commentbegin2.concat(action.payload).concat(commentend2)
-             let newobjectpost2 = {...desiredpost2, comments: newcomment2}
-             let findindex2 = state.posts.findIndex(post => post.id === action.payload.post_id)
-             let begslice2 = state.posts.slice(0, findindex2)
-             let endslice2 = state.posts.slice(findindex2 + 1)
-             let allposts2 = begslice2.concat(newobjectpost2).concat(endslice2)
-             return {...state, posts: allposts2}
-        case 'DELETE_COMMENT':
-            let postdeleteobject = state.posts.filter(post => post.id === action.payload.post_id)[0]
-            let updatedcommentpostdeleteobject = postdeleteobject.comments.filter(comment => comment.id != action.payload.id)
-            let newupdatedcommentpost = {...postdeleteobject, comments: updatedcommentpostdeleteobject}
-            let indexofpost = state.posts.findIndex(post => post.id === action.payload.post_id)
-            let indexslicebegin = state.posts.slice(0, indexofpost)
-            let indexsliceend = state.posts.slice(indexofpost + 1)
-            let newlydeletedpostobject = indexslicebegin.concat(newupdatedcommentpost).concat(indexsliceend)
-            return {...state, posts: newlydeletedpostobject}
+            let editOriginalPostIndex = state.posts.findIndex(post => post.id === action.payload.id)
+            let editUpdatedPost = state.posts.slice(0, editOriginalPostIndex).concat(action.payload).concat(state.posts.slice(editOriginalPostIndex + 1))
+            return {...state, posts: editUpdatedPost}
         case 'DELETE_POST':
-            let newfilteredpost = state.posts.filter(post => post.id != action.payload.id)
-            return {...state, posts: newfilteredpost}
+            let deleteUpdatedPost = state.posts.filter(post => post.id != action.payload.id)
+            return {...state, posts: deleteUpdatedPost}
+        case 'ADD_COMMENT':
+            let addCommentOriginalPost = state.posts.filter(post => post.id === action.payload.post_id)[0]
+            let addCommentUpdatedPost = {...addCommentOriginalPost, upvotes: action.payload.post.upvotes, comments: [...addCommentOriginalPost.comments, action.payload]}
+            let addCommentOriginalPostIndex = state.posts.findIndex(post => post.id === action.payload.post_id)
+            let addCommentUpdatedPosts = state.posts.slice(0, addCommentOriginalPostIndex).concat(addCommentUpdatedPost).concat(state.posts.slice(addCommentOriginalPostIndex + 1))
+            return {...state, posts: addCommentUpdatedPosts}
+        case 'EDIT_COMMENT':
+            let editCommentOriginalPost = state.posts.filter(post => post.id === action.payload.post_id)[0]
+            let editCommentIndex = editCommentOriginalPost.comments.findIndex(comment => comment.id === action.payload.id)
+            let editUpdatedComment = editCommentOriginalPost.comments.slice(0, editCommentIndex).concat(action.payload).concat(editCommentOriginalPost.comments.slice(editCommentIndex + 1))
+            let editCommentUpdatedPost = {...editCommentOriginalPost, comments: editUpdatedComment}
+            let editCommentOriginalPostIndex = state.posts.findIndex(post => post.id === action.payload.post_id)
+            let editCommentUpdatedPosts = state.posts.slice(0, editCommentOriginalPostIndex).concat(editCommentUpdatedPost).concat(state.posts.slice(editCommentOriginalPostIndex + 1))
+            return {...state, posts: editCommentUpdatedPosts}
+        case 'DELETE_COMMENT':
+            let deleteCommentOriginalPost = state.posts.filter(post => post.id === action.payload.post_id)[0]
+            let deleteComment = deleteCommentOriginalPost.comments.filter(comment => comment.id != action.payload.id)
+            let deleteCommentUpdatedPost = {...deleteCommentOriginalPost, comments: deleteComment}
+            let deleteCommentOriginalPostIndex = state.posts.findIndex(post => post.id === action.payload.post_id)
+            let deleteCommentUpdatedPosts = state.posts.slice(0, deleteCommentOriginalPostIndex).concat(deleteCommentUpdatedPost).concat(state.posts.slice(deleteCommentOriginalPostIndex + 1))
+            return {...state, posts: deleteCommentUpdatedPosts}
         default:
             return state
     }
