e8776500c50b0acc3707367e6bca76955cc83769
Yvonne <yvonnersz@gmail.com>
2021-01-22 22:49:16 -0800

    Add div class and associated css to Registration component

--- src/components/users/UserInput.js
+++ src/components/users/UserInput.js
@@ -1,73 +1,74 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class UserInput extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: '',
-            password: '',
-            password_confirmation: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      password_confirmation: "",
+    };
+  }
 
-    handleOnChange = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-        const {username, password, password_confirmation} = this.state
+    const { username, password, password_confirmation } = this.state;
 
-        axios.post('http://localhost:3000/users', {
-            user: {
-                username: username,
-                password: password,
-                password_confirmation: password_confirmation
-            }
-        }, 
-        {withCredentials: true}
-        )
+    axios.post(
+      "http://localhost:3000/users",
+      {
+        user: {
+          username: username,
+          password: password,
+          password_confirmation: password_confirmation,
+        },
+      },
+      { withCredentials: true }
+    );
 
-        this.props.history.push('/')
-    }
+    this.props.history.push("/");
+  };
 
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <input 
-                        type='text'
-                        name='username'
-                        placeholder='Enter in a username'
-                        onChange={this.handleOnChange}>
-                    </input>
+  render() {
+    return (
+      <div class="card">
+        <div class="card-body">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter in a username"
+              onChange={this.handleOnChange}
+            ></input>
 
-                    <input
-                        type='password'
-                        name='password'
-                        placeholder='Enter password'
-                        onChange={this.handleOnChange}>
-                    </input>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={this.handleOnChange}
+            ></input>
 
-                    <input
-                        type='password'
-                        name='password_confirmation'
-                        placeholder='Enter password'
-                        onChange={this.handleOnChange}>
-                    </input>
+            <input
+              type="password"
+              name="password_confirmation"
+              placeholder="Enter password"
+              onChange={this.handleOnChange}
+            ></input>
 
-                    <input
-                        type='submit'
-                        value='Register'>
-                    </input>
-                </form>
-            </div>
-        )
-    }
+            <input type="submit" value="Register"></input>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserInput;
