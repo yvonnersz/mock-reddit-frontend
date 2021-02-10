41e93d1577f061705e4258f8211e5e5ea6d0d666
Yvonne <yvonnersz@gmail.com>
2021-02-09 18:25:09 -0800

    Add BootStrap div class for automatic styling

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -1,70 +1,117 @@
-import React from 'react';
-import Pluralize from 'react-pluralize';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom';
+import React from "react";
+import Pluralize from "react-pluralize";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-import {editPost} from '../../actions/post/editPost';
-import {deletePost} from '../../actions/post/deletePost';
+import { editPost } from "../../actions/post/editPost";
+import { deletePost } from "../../actions/post/deletePost";
 
 class Post extends React.Component {
-    
-    handleVote = (event, post) => {
-        if (event.target.name === "upvote") {
-            if (post.toggle_downvote === true) {
-                let updatePost = {...post, upvotes: post.upvotes + 1, toggle_upvote: false, toggle_downvote: false}
-                this.props.editPost(updatePost)
-            } else {
-                let updatePost = {...post, upvotes: post.upvotes + 1, toggle_upvote: true, toggle_downvote: false}
-                this.props.editPost(updatePost)
-            }
-        } else if (event.target.name === "downvote") {
-            if (post.toggle_upvote === true) {
-                let updatePost = {...post, upvotes: post.upvotes - 1, toggle_upvote: false, toggle_downvote: false}
-                this.props.editPost(updatePost)
-            } else {
-                let updatePost = {...post, upvotes: post.upvotes - 1, toggle_upvote: false, toggle_downvote: true}
-                this.props.editPost(updatePost)
-            }
-        }
+  handleVote = (event, post) => {
+    if (event.target.name === "upvote") {
+      if (post.toggle_downvote === true) {
+        let updatePost = {
+          ...post,
+          upvotes: post.upvotes + 1,
+          toggle_upvote: false,
+          toggle_downvote: false,
+        };
+        this.props.editPost(updatePost);
+      } else {
+        let updatePost = {
+          ...post,
+          upvotes: post.upvotes + 1,
+          toggle_upvote: true,
+          toggle_downvote: false,
+        };
+        this.props.editPost(updatePost);
+      }
+    } else if (event.target.name === "downvote") {
+      if (post.toggle_upvote === true) {
+        let updatePost = {
+          ...post,
+          upvotes: post.upvotes - 1,
+          toggle_upvote: false,
+          toggle_downvote: false,
+        };
+        this.props.editPost(updatePost);
+      } else {
+        let updatePost = {
+          ...post,
+          upvotes: post.upvotes - 1,
+          toggle_upvote: false,
+          toggle_downvote: true,
+        };
+        this.props.editPost(updatePost);
+      }
     }
+  };
 
-    handleDelete = (post) => {
-        this.props.deletePost(post)
-        this.props.history.push('/posts')
+  handleDelete = (post) => {
+    this.props.deletePost(post);
+    this.props.history.push("/posts");
+  };
+
+  dateFormat = (post) => {
+    if (post) {
+      return new Date(post.created_at).toDateString();
     }
+  };
 
-    dateFormat = (post) => {
-        if (post) {
-            return new Date(post.created_at).toDateString()
-        }
-    }
+  render() {
+    let post = this.props.posts.filter(
+      (post) => post.id === parseInt(this.props.match.params.id)
+    )[0];
 
-    render() {
+    return (
+      <div class="card">
+        <div class="upvotes-column">
+          <button
+            name="upvote"
+            onClick={(event) => this.handleVote(event, post)}
+          >
+            ⇧
+          </button>
+          <br />{" "}
+          {post.votes
+            ? post.votes.filter((vote) => vote.upvote === true).length -
+              post.votes.filter((vote) => vote.downvote === true).length
+            : 0}
+          <br />
+          <button
+            name="downvote"
+            onClick={(event) => this.handleVote(event, post)}
+          >
+            ⇩
+          </button>
+          <br />
+        </div>
 
-        let post = this.props.posts.filter(post => post.id === parseInt(this.props.match.params.id))[0]
+        <div class="card-body">
+          <div class="card-header">
+            r/{post.subreddit} • Posted by u/{post.user.username} •
+            {this.dateFormat(post)}
+          </div>
 
-        return (
-            <div class='container-fluid text-center'>
-                <div class='row content'>
+          <div class="card-title">
+            <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          </div>
 
-                    <div class='upvotes-column'>
-                        <button name='upvote' onClick={(event) => this.handleVote(event, post)} disabled={post.toggle_upvote === true ? 'true':''}>⇧</button><br/> {post.upvotes}<br/>
-                        <button name='downvote' onClick={(event) => this.handleVote(event, post)} disabled={post.toggle_downvote === true ? 'true':''}>⇩</button><br/>
-                    </div>
+          <div class="card-text">
+            <Link to={`/posts/${post.id}`}>
+              <p>{post.content}</p>
+            </Link>
+          </div>
 
-                    <div class='col-lg-8 text-left'>
-                        <span>r/{post ? post.subreddit:null} • u/{post ? post.user.username:null} • {this.dateFormat(post)}</span><br/>
-                        <h3>{post ? post.title:null}</h3>
-                        <p>{post ? post.content:null}</p>
-                        <span><Link to={`/posts/${post ? post.id:null}/comments/`}><Pluralize singular={'Comment'} count={post ? post.comments.length:null} /></Link></span> &nbsp;
-                        <span><Link to={`/posts/${post ? post.id:null}/edit`}>Edit</Link></span> &nbsp;
-                        <span onClick={(event) => this.handleDelete(post)}><Link to={`/posts/${post ? post.id:null}`}>Delete</Link></span>
-                    </div>
-
-                </div>
-            </div>
-        )
-    }
+          <div class="card-footer">
+            <Link to={`/posts/${post.id}/comments`}>
+              <Pluralize singular={"Comment"} count={post.comments.length} />
+            </Link>
+          </div>
+        </div>        
+      </div>
+    );
+  }
 }
 
-export default connect(null, {editPost, deletePost})(Post)
+export default connect(null, { editPost, deletePost })(Post);
