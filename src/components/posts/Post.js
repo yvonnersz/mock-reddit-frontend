cd2068ce1f9703aa495bb2ee3a4bdc6ebb8fa37c
Yvonne <yvonnersz@gmail.com>
2021-02-16 20:52:49 -0800

    Fix user vote bug

--- src/components/posts/Post.js
+++ src/components/posts/Post.js
@@ -13,37 +13,44 @@ class Post extends React.Component {
     super();
     this.state = {
       upvotePress: false,
-      upvotePress: false
+      upvotePress: false,
     };
   }
 
   handleVote = (event, post) => {
-    let vote = post.votes.filter(vote => vote.user_id === this.props.user.id && (vote.upvote === true || vote.downvote === true))[0]
+    let vote = post.votes.filter(
+      (vote) =>
+        vote.user_id === this.props.user.id &&
+        (vote.upvote === true || vote.downvote === true)
+    )[0];
 
-    if (vote && event.target.getAttribute('aria-pressed') === 'true' || vote && event.target.getAttribute('aria-pressed') === 'false') {
+    if (
+      (vote && event.target.getAttribute("aria-pressed") === "true") ||
+      (vote && event.target.getAttribute("aria-pressed") === "false")
+    ) {
       let deleteVote = {
         post_id: vote.post_id,
-        user_id: this.props.user ? this.props.user.id : null
-      }
-      this.props.deleteVote(vote)
-    } else if (event.target.name === 'upvote') {
+        user_id: this.props.user ? this.props.user.id : null,
+      };
+      this.props.deleteVote(vote);
+    } else if (event.target.name === "upvote") {
       let upvotePost = {
         ...post,
         upvote: true,
         downvote: false,
-        user_id: this.props.user.id
-      }
-  
-      this.props.addVote(upvotePost, post.id)
-    } else if (event.target.name === 'downvote') {
+        user_id: this.props.user.id,
+      };
+
+      this.props.addVote(upvotePost, post.id);
+    } else if (event.target.name === "downvote") {
       let downvotePost = {
         ...post,
         upvote: false,
         downvote: true,
-        user_id: this.props.user.id
-      }
-  
-      this.props.addVote(downvotePost, post.id)
+        user_id: this.props.user.id,
+      };
+
+      this.props.addVote(downvotePost, post.id);
     }
   };
 
@@ -53,28 +60,27 @@ class Post extends React.Component {
   };
 
   dateFormat = (post) => {
-    let date1 = new Date(post.created_at)
-    let date2 = new Date()
+    let date1 = new Date(post.created_at);
+    let date2 = new Date();
 
     let diffDate = date2.getTime() - date1.getTime();
-    let diffSecs = diffDate / (1000)
+    let diffSecs = diffDate / 1000;
 
     if (diffSecs < 60) {
-      return Math.ceil(diffSecs) + ' sec ago'
-    } else if ((diffSecs/60) < 60) {
-      return Math.ceil(diffSecs/60) + ' mins ago'
-    } else if (diffSecs/(60*60) < 24) {
-      return Math.ceil(diffSecs/(60*60)) + ' hours ago'
-    } else if (diffSecs/(60*60*24) < 7) {
-      return Math.ceil(diffSecs/(60*60*24)) + ' days ago'
-    } else if (diffSecs/(60*60*24*7) < 4) {
-      return (Math.ceil(diffSecs/(60*60*24*7))) + ' weeks ago'
-    } else if (diffSecs/(60*60*24*7*4) < 12) {
-      return (Math.ceil(diffSecs/(60*60*24*7*4))) + ' months ago'
+      return Math.ceil(diffSecs) + " sec ago";
+    } else if (diffSecs / 60 < 60) {
+      return Math.ceil(diffSecs / 60) + " mins ago";
+    } else if (diffSecs / (60 * 60) < 24) {
+      return Math.ceil(diffSecs / (60 * 60)) + " hours ago";
+    } else if (diffSecs / (60 * 60 * 24) < 7) {
+      return Math.ceil(diffSecs / (60 * 60 * 24)) + " days ago";
+    } else if (diffSecs / (60 * 60 * 24 * 7) < 4) {
+      return Math.ceil(diffSecs / (60 * 60 * 24 * 7)) + " weeks ago";
+    } else if (diffSecs / (60 * 60 * 24 * 7 * 4) < 12) {
+      return Math.ceil(diffSecs / (60 * 60 * 24 * 7 * 4)) + " months ago";
     } else {
-      return (Math.ceil(diffSecs/(60*60*24*7*4*12))) + ' years ago'
+      return Math.ceil(diffSecs / (60 * 60 * 24 * 7 * 4 * 12)) + " years ago";
     }
-
   };
 
   render() {
@@ -82,36 +88,83 @@ class Post extends React.Component {
 
     if (this.props.match) {
       let id = this.props.match.params.id;
-      post = this.props.posts.filter(post => post.id === parseInt(id))[0];
+      post = this.props.posts.filter((post) => post.id === parseInt(id))[0];
     } else {
-      post = this.props.post
+      post = this.props.post;
     }
-    
+
     return (
       <div class="card post">
         <div class="upvotes-column">
           <div class="upvotes-buttons">
-            <button
-              aria-pressed={post.votes.filter(vote => vote.user_id === this.props.user.id && vote.upvote === true)[0] ? true:false}
-              name="upvote"
-              onClick={(event) => this.handleVote(event, post)}
-            > 🡅
-            </button>
-            <br/>
-          
-              <div class="upvotes">
-                {post.votes ? post.votes.filter(vote => vote.upvote === true).length - post.votes.filter(vote => vote.downvote === true).length:0} <br/>
-              </div>
+            {this.props.user ? (
+              <button
+                aria-pressed={
+                  post.votes.filter(
+                    (vote) =>
+                      vote.user_id === this.props.user.id &&
+                      vote.upvote === true
+                  )[0]
+                    ? true
+                    : false
+                }
+                name="upvote"
+                onClick={(event) => this.handleVote(event, post)}
+              >
+                {" "}
+                🡅
+              </button>
+            ) : (
+              <button
+                aria-pressed="false"
+                name="upvote"
+                onClick={(event) => this.handleVote(event, post)}
+              >
+                🡅
+              </button>
+            )}
 
-            <button
-              aria-pressed={post.votes.filter(vote => vote.user_id === this.props.user.id && vote.downvote === true)[0] ? true:false}
-              name="downvote"
-              onClick={(event) => this.handleVote(event, post)}
-            > 🡇
-            </button>
-            <br/>
+            <br />
+
+            <div class="upvotes">
+              {post.votes
+                ? post.votes.filter((vote) => vote.upvote === true).length -
+                  post.votes.filter((vote) => vote.downvote === true).length
+                : 0}{" "}
+              <br />
+            </div>
+
+            {this.props.user ? (
+              <button
+                aria-pressed={
+                  post.votes.filter(
+                    (vote) =>
+                      vote.user_id === this.props.user.id &&
+                      vote.downvote === true
+                  )[0]
+                    ? true
+                    : false
+                }
+                name="downvote"
+                onClick={(event) => this.handleVote(event, post)}
+              >
+                {" "}
+                🡇
+              </button>
+            ) : (
+              <button
+                aria-pressed="false"
+                name="downvote"
+                onClick={(event) => this.handleVote(event, post)}
+              >
+                {" "}
+                🡇
+              </button>
+            )}
+
+            <br />
           </div>
-        </div>   
+        </div>
 
         <div class="card-body">
           <div class="card-header">
@@ -131,16 +184,15 @@ class Post extends React.Component {
 
           <div class="card-footer">
             <Link to={`/posts/${post.id}/comments`}>
-              <Pluralize
-                singular={"Comment"}
-                count={post.comments.length}
-              />
+              <Pluralize singular={"Comment"} count={post.comments.length} />
             </Link>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect(null, { addVote, editPost, deletePost, deleteVote })(Post);
+export default connect(null, { addVote, editPost, deletePost, deleteVote })(
+  Post
+);
