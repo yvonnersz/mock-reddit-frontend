5122eca9b6d3f6d7dfa88e1a0bd2a6cfbd21e049
Yvonne <yvonnersz@gmail.com>
2021-02-25 13:21:57 -0800

    Delete unused code

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -19,46 +19,6 @@ class Posts extends React.Component {
     };
   }
 
-  setData() {
-    sessionStorage.setItem('posts', JSON.stringify(this.props.posts))
-  }
-
-  handleVote = (event, post) => {
-    let vote = post.votes.filter(vote => vote.user_id === this.props.user.id && (vote.upvote === true || vote.downvote === true))[0]
-
-    if (vote && event.target.getAttribute('aria-pressed') === 'true' || vote && event.target.getAttribute('aria-pressed') === 'false') {
-      let deleteVote = {
-        post_id: vote.post_id,
-        user_id: this.props.user ? this.props.user.id : null
-      }
-      this.props.deleteVote(vote)
-    } else if (event.target.name === 'upvote') {
-      let upvotePost = {
-        ...post,
-        upvote: true,
-        downvote: false,
-        user_id: this.props.user.id
-      }
-  
-      this.props.addVote(upvotePost, post.id)
-    } else if (event.target.name === 'downvote') {
-      let downvotePost = {
-        ...post,
-        upvote: false,
-        downvote: true,
-        user_id: this.props.user.id
-      }
-  
-      this.props.addVote(downvotePost, post.id)
-    }
-  };
-
-  dateFormat = (post) => {
-    if (post) {
-      return new Date(post.created_at).toDateString();
-    }
-  };
-
   handleBySort = (event) => {
     this.setState({
       [event.target.name]: !this.state[event.target.name]
@@ -66,8 +26,6 @@ class Posts extends React.Component {
   };
 
   render() {
-    this.setData();
-
     const sortPosts = [...this.props.posts].sort((aPost, bPost) => (aPost.votes.filter(vote => vote.upvote === true).length - aPost.votes.filter(vote => vote.downvote === true).length) - (bPost.votes.filter(vote => vote.upvote === true).length - bPost.votes.filter(vote => vote.downvote === true).length)).reverse()
 
     return (
@@ -98,8 +56,6 @@ class Posts extends React.Component {
           </div>
         </div>
         
-        {/* <button onClick={this.handleBySort}>Sort by Votes</button> */}
-
         {(this.state.best === false ? this.props.posts : sortPosts).map(
           post => <Post key={post.id} post={post} user={this.props.user} posts={this.props.posts} />)}
            </div>
