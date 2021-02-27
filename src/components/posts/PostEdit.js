da3750809b3819a43a0c84f171cee6630f685463
Yvonne <yvonnersz@gmail.com>
2021-02-26 16:37:52 -0800

    Add classes for BootStrap stylization

--- src/components/posts/PostEdit.js
+++ src/components/posts/PostEdit.js
@@ -1,47 +1,53 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import {editPost} from '../../actions/post/editPost';
+import { editPost } from "../../actions/post/editPost";
 
 class PostInput extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            content: '',
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      content: "",
+    };
+  }
 
-    handleOnChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
+  handleOnChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    handleOnSubmit = event => {
-        event.preventDefault();
-        let post = {...this.state, id: this.props.match.params.id}
-        this.props.editPost(post)
-        this.setState({
-            content: ''
-        })
-        this.props.history.push(`/posts/${this.props.match.params.id}`)
-    }
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    let post = { ...this.state, id: this.props.match.params.id };
+    this.props.editPost(post);
+    this.setState({
+      content: "",
+    });
+    this.props.history.push(`/posts/${this.props.match.params.id}`);
+  };
 
-    render() {
-        return (
-            <div class='container-fluid text-center'>
-                <h2>Edit a Post</h2>
-                <div class='row content new-post'>
-                    <div class='form-group'>
-                        <form onSubmit={this.handleOnSubmit}>
-                            <textarea rows='7' placeholder='Content' value={this.state.content} name='content' onChange={this.handleOnChange} class="form-control" /><br/>
-                            <input type='submit' value="Edit Post" /><br/>
-                        </form>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div class="container-fluid bg-white rounded m-3 p-3 mx-auto text-center">
+        
+        <h4>Edit Post</h4>
+
+        <div class="form-group">
+          <form onSubmit={this.handleOnSubmit}>
+
+            <input type="text" class="form-control mb-1" placeholder="Title" value={this.state.title} name="title" onChange={this.handleOnChange} />
+            <textarea rows="7" class="form-control" placeholder="Content" value={this.state.content} name="content" onChange={this.handleOnChange} />
+
+            <div class="d-grid mt-1">
+              <input type="submit" class="btn btn-primary" value="Edit Post" />
             </div>
-        )
-    }
+
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default connect(null, {editPost})(PostInput);
+export default connect(null, { editPost })(PostInput);
