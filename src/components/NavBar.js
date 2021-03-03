19a841249ed2d74c251fbd8d3eefd5cbe4dfabe8
Yvonne <yvonnersz@gmail.com>
2021-03-02 19:46:16 -0800

    Create user modal

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import UserLogin from './users/UserLogin';
 
 const NavBar = props => {
   let register = !props.user ? 'Register' : null;
@@ -7,23 +8,30 @@ const NavBar = props => {
   let logout = props.user ? 'Logout' : null;
 
   return (
+    <>
     <nav class="navbar sticky-top bg-light">
 
       <Link to="/">Home</Link>
 
-      <Link to='/register'>
-        <span>{register}</span>
-      </Link>
-
-      <Link to='/login'>
+      {/* <Link to='/login'>
         <span>{login}</span>
+      </Link> */}
+
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#login-modal">
+        Log In
+      </button>
+
+      {/* <Link to='/register'>
+        <span>{register}</span>
       </Link>
 
       <Link to='/login' onClick={props.handleLogOut}>
         <span>{logout}</span>
-      </Link>
+      </Link> */}
 
     </nav>
+    <UserLogin />
+    </>
   )
 }
 
--- src/components/users/UserLogin.js
+++ src/components/users/UserLogin.js
@@ -1,12 +1,11 @@
 import React from "react";
-import axios from "axios";
 
 class UserLogin extends React.Component {
   constructor() {
     super();
     this.state = {
       username: "",
-      password: ""
+      password: "",
     };
   }
 
@@ -18,45 +17,43 @@ class UserLogin extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-
     const { username, password } = this.state;
-
-    this.props.handleLogin(username, password)
-    // axios.post(
-    //   "http://localhost:3000/sessions",
-    //   {
-    //     user: {
-    //       username: username,
-    //       password: password
-    //     },
-    //   },
-    //   { withCredentials: true }
-    // );
-
+    this.props.handleLogin(username, password);
     this.props.history.push("/");
   };
 
   render() {
     return (
-      <div class="card">
-        <div class="card-body">
-          <form onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="username"
-              placeholder="Enter username"
-              onChange={this.handleOnChange}
-            ></input>
+      <div class="modal fade" id="login-modal" tabindex="-1"  aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
 
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter password"
-              onChange={this.handleOnChange}
-            ></input>
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">
+                Login
+              </h5>
 
-            <input type="submit" value="Login"></input>
-          </form>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+              <div class='form-group'>
+                <form onSubmit={this.handleSubmit}>
+
+                  <input type='text' class='form-control mb-1' name="username" placeholder="Enter username" onChange={this.handleOnChange} />
+                  <input type='password' class='form-control mb-1' name="password" placeholder="Enter password" onChange={this.handleOnChange} />
+                  
+                </form>
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button type="submit" class="btn btn-primary" data-bs-dismiss="modal">
+                Login
+              </button>
+            </div>
+
+          </div>
         </div>
       </div>
     );
