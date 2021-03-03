dbeb073d9a631647e50c3f7160adf3ad36c9cd52
Yvonne <yvonnersz@gmail.com>
2021-03-02 21:23:19 -0800

    Remove logic from return function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,53 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import UserLogin from './users/UserLogin';
 import UserInput from './users/UserInput';
 
 const NavBar = props => {
-  let register = !props.user ? 'Register' : null;
-  let login = !props.user ? 'Login' : null;
-  let logout = props.user ? 'Logout' : null;
+  let register = !props.user ? <button type='button' class='btn btn-primary rounded-pill pl-5 pr-5' data-bs-toggle='modal' data-bs-target='#signup-modal'> Sign Up </button> : null;
+  let login = !props.user ? <button type='button' class='btn btn-outline-primary rounded-pill pl-5 pr-5' data-bs-toggle='modal' data-bs-target='#login-modal'> Log In </button> : null;
+  let logout = props.user ? <button type='button' class='btn btn-outline-primary rounded-pill pl-5 pr-5' onClick={props.handleLogOut}> Log Out </button> : null;
 
   return (
     <>
-    <nav class="navbar sticky-top bg-light">
+      <nav class='navbar sticky-top bg-light'>
 
-      <Link to="/">Home</Link>
+        <Link to='/'>Home</Link>
 
-      {/* <Link to='/login'>
-        <span>{login}</span>
-      </Link> */}
+        <div class='d-grid gap-2 d-md-flex justify-content-md-end'>
+          {login}
+          {register}
+          {logout}
+        </div>
 
-<div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      </nav>
 
-
-      <button type="button" class="btn btn-outline-primary rounded-pill pl-5 pr-5" data-bs-toggle="modal" data-bs-target="#login-modal">
-        Log In
-      </button>
-
-      <button type="button" class="btn btn-primary rounded-pill pl-5 pr-5" data-bs-toggle="modal" data-bs-target="#signup-modal">
-        Sign Up
-      </button>
-
-      <button type="button" class="btn btn-outline-primary rounded-pill pl-5 pr-5" onClick={props.handleLogOut}>
-        Log Out
-      </button>
-      </div>
-
-      {/* <Link to='/register'>
-        <span>{register}</span>
-      </Link>
-
-      <Link to='/login' onClick={props.handleLogOut}>
-        <span>{logout}</span>
-      </Link> */}
-
-    </nav>
-    <UserLogin handleLogin={props.handleLogin} />
-    <UserInput />
+      <UserLogin handleLogin={props.handleLogin} />
+      <UserInput />
     </>
   )
 }
-
 
 export default NavBar;
