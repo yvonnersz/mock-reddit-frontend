a6d25fb32bcfab8df1fa4c25e4a16548ecedbf8d
Yvonne <yvonnersz@gmail.com>
2021-03-10 22:07:23 -0800

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-
 import App from './App';
 import postsReducer from './reducers/postsReducer';
 
