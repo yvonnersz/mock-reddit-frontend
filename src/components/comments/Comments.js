b4b8c4f1ec39ed0f8a0748e5904ab4f378a30edd
Yvonne <yvonnersz@gmail.com>
2021-03-10 21:01:03 -0800

    Refactor <Comments />

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Comment from "./Comment";
+import React from 'react';
+import Comment from './Comment';
 
 const Comments = ({ user, posts, idPost }) => {
-  let post = idPost ? posts.filter((post) => post.id === parseInt(idPost))[0] : null;
+  let post = idPost ? posts.filter(post => post.id === parseInt(idPost))[0] : null;
 
   return (
     <>
-      { post.comments.reverse().map(comment => <Comment key={comment.id} user={user} post={post} comment={comment} />) }
+      { post.comments.map(comment => <Comment key={comment.id} user={user} post={post} comment={comment} />) }
     </>
-  );
-};
+  )
+}
 
 export default Comments;
