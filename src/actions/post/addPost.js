fbeb004884336095f2e1d5dba4f63b6da038c094
Yvonne <yvonnersz@gmail.com>
2021-03-10 21:53:44 -0800

    Fix spacing of addPost action

--- src/actions/post/addPost.js
+++ src/actions/post/addPost.js
@@ -1,25 +1,22 @@
 export const addPost = (post, props) => {
-    return(dispatch) => {
-        fetch('http://localhost:3000/api/v1/posts', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(post)
-        })
-        .then(response => response.json())
-        .then(json => {
-            if (json.error) {
-                alert(json.error)
-            } else {
-                let newPost = {...json, user: props.user}
-                dispatch({ type: 'ADD_POST', payload: newPost })
-                props.history.push({
-                    pathname: `/posts/${json.id}/comments`,
-                    new: true
-                })
-            }
-        })
-    }
+  return dispatch => {
+    fetch('http://localhost:3000/api/v1/posts', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(post)
+    })
+    .then(response => response.json())
+    .then(json => {
+      if (json.error) {
+        alert(json.error)
+      } else {
+        let newPost = {...json, user: props.user}
+        dispatch({ type: 'ADD_POST', payload: newPost })
+        props.history.push({ pathname: `/posts/${json.id}/comments`, new: true })
+      }
+    })
+  }
 }
