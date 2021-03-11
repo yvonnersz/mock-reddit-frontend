9d38302e55c936b51b9910825be44c89480d4fb0
Yvonne <yvonnersz@gmail.com>
2021-03-10 21:38:04 -0800

    Fix spacing

--- src/containers/PostsContainer.js
+++ src/containers/PostsContainer.js
@@ -17,21 +17,21 @@ class PostsContainer extends React.Component {
   render() {
     return (
       <div>
-          <NavBar user={this.props.user} handleLogin={this.props.handleLogin} handleLogOut={this.props.handleLogOut} />
+        <NavBar user={this.props.user} handleLogin={this.props.handleLogin} handleLogOut={this.props.handleLogOut} />
 
-          <Switch>
-            <Route exact path='/' render= {routerProps => <Posts {...routerProps} posts={this.props.posts} user={this.props.user} />} />
-            <Route exact path='/posts/new' render={routerProps => <PostInput {...routerProps} user={this.props.user} />} />
-            <Route path='/register' render={routerProps => <UserInput {...routerProps} />} />
-            <Route path='/login' render={routerProps => <UserLogin {...routerProps} handleLogin={this.props.handleLogin} />} />
-            <Route path='/posts/:id/comments/:id' render={routerProps => <CommentEdit {...routerProps} posts={this.props.posts}/>} />
-            <Route path='/posts/:id/comments' render={routerProps => <> <Post {...routerProps} posts={this.props.posts} user={this.props.user} /> <CommentsContainer {...routerProps} posts={this.props.posts} user={this.props.user}/> </> } />
-            <Route path='/posts/:id/edit' render={routerProps => <PostEdit {...routerProps} posts={this.props.posts} />} />
-            <Route path='/posts/:id' render={routerProps => <><Post {...routerProps} posts={this.props.posts} user={this.props.user}/></>} />
-          </Switch>
+        <Switch>
+          <Route exact path='/' render= {routerProps => <Posts {...routerProps} posts={this.props.posts} user={this.props.user} />} />
+          <Route exact path='/posts/new' render={routerProps => <PostInput {...routerProps} user={this.props.user} />} />
+          <Route path='/register' render={routerProps => <UserInput {...routerProps} />} />
+          <Route path='/login' render={routerProps => <UserLogin {...routerProps} handleLogin={this.props.handleLogin} />} />
+          <Route path='/posts/:id/comments/:id' render={routerProps => <CommentEdit {...routerProps} posts={this.props.posts}/>} />
+          <Route path='/posts/:id/comments' render={routerProps => <> <Post {...routerProps} posts={this.props.posts} user={this.props.user} /> <CommentsContainer {...routerProps} posts={this.props.posts} user={this.props.user}/> </> } />
+          <Route path='/posts/:id/edit' render={routerProps => <PostEdit {...routerProps} posts={this.props.posts} />} />
+          <Route path='/posts/:id' render={routerProps => <><Post {...routerProps} posts={this.props.posts} user={this.props.user}/></>} />
+        </Switch>
 
-          <UserLogin handleLogin={this.props.handleLogin} user={this.props.user} loginError={this.props.loginError}/>
-          <UserInput />
+        <UserLogin handleLogin={this.props.handleLogin} user={this.props.user} loginError={this.props.loginError}/>
+        <UserInput />
       </div>
     )
   }
