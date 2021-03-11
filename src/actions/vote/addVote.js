70784498b85dbd196de4b32b24dbb8f34a6fb9db
Yvonne <yvonnersz@gmail.com>
2021-03-10 21:45:39 -0800

    Refactor addVote action

--- src/actions/vote/addVote.js
+++ src/actions/vote/addVote.js
@@ -1,20 +1,20 @@
 export const addVote = (vote, postId) => {
-    return(dispatch) => {
-        fetch(`http://localhost:3000/api/v1/posts/${postId}/votes`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify({vote})
-        })
-        .then(response => response.json())
-        .then(json => {
-            if (json.error) {
-                alert(json.error)
-            } else {
-                dispatch({ type: 'ADD_VOTE', payload: json })
-            }
-        }) 
-    }
+  return dispatch => {
+    fetch(`http://localhost:3000/api/v1/posts/${postId}/votes`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({vote})
+    })
+    .then(response => response.json())
+    .then(json => {
+      if (json.error) {
+        alert(json.error)
+      } else {
+        dispatch({ type: 'ADD_VOTE', payload: json })
+      }
+    }) 
+  }
 }
