1f1eeca89a78035cab95ce3fa2eabb6e2548325f
Yvonne <yvonnersz@gmail.com>
2021-03-10 21:48:42 -0800

    Refactor deleteCommentVote action

--- src/actions/vote/deleteCommentVote.js
+++ src/actions/vote/deleteCommentVote.js
@@ -1,13 +1,13 @@
-export const deleteCommentVote = (vote) => {
-    return(dispatch) => {
-        fetch(`http://localhost:3000/api/v1/comments/${vote.comment_id}/votes/${vote.id}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            method: 'DELETE',
-        })
-        .then(response => response.json())
-        .then(json => dispatch({ type: 'DELETE_COMMENT_VOTE', payload: json }))
-    }
+export const deleteCommentVote = vote => {
+  return(dispatch) => {
+    fetch(`http://localhost:3000/api/v1/comments/${vote.comment_id}/votes/${vote.id}`, {
+      headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+      },
+      method: 'DELETE',
+    })
+    .then(response => response.json())
+    .then(json => dispatch({ type: 'DELETE_COMMENT_VOTE', payload: json }))
+  }
 }
