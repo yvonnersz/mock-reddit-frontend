b349041878ca65461ea59b7b77f505e220367b63
Yvonne <yvonnersz@gmail.com>
2021-03-10 21:39:41 -0800

    Refactor <CommentsContainer />

--- src/containers/CommentsContainer.js
+++ src/containers/CommentsContainer.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import CommentInput from '../components/comments/CommentInput';
+import Comments from '../components/comments/Comments';
 
-import CommentInput from '../components/comments/CommentInput'
-import Comments from '../components/comments/Comments'
-
-const CommentsContainer = (props) => {
-    return (
-        <div>
-            <CommentInput posts={props.posts} id={props.match.params.id} user={props.user}/>
-            <Comments posts={props.posts} idPost={props.match.params.id} user={props.user}/>
-        </div>
-    )
+const CommentsContainer = props => {
+  return (
+    <div>
+      <CommentInput posts={props.posts} id={props.match.params.id} user={props.user}/>
+      <Comments posts={props.posts} idPost={props.match.params.id} user={props.user}/>
+    </div>
+  )
 }
 
-export default CommentsContainer
+export default CommentsContainer;
